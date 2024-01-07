ab5046529c35beff7626fc00b61dfbfec9b2fa9d
Akash Bag <110753356+akash0708@users.noreply.github.com>
2024-01-07 12:24:31 +0530

    fix: mongo error fixed

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,7 @@ import mongoose, { mongo } from "mongoose";
 let isConnected = false;
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery')
+    mongoose.set('strictQuery', true)
     if(isConnected) {
         console.log('Mongoose is already conected')
         return;
